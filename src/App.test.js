d3adcf59bef780c86e3f43174d5a119d1127ada3
dylan909 <73878448+dylan909@users.noreply.github.com>
2023-05-07 18:03:17 +0100

    fixing tests

--- src/App.test.js
+++ src/App.test.js
@@ -8,9 +8,6 @@ import { App } from "./App";
 
 describe("tests", () => {
   it("should do something amazing", () => {
-    expect(true).toBe(false);
-  });
-  it("should do something truely amazing", () => {
     expect(true).toBe(true);
   });
 });
